e5c8bd2135791cbc58ef115d33d8fde0de885380
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-07-30 22:42:21 -0230

    Working on login form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./Components/Home";
 import Registration from "./Components/Registration";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Registration />
+      <Login />
     </div>
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+   
+    
+      <div className='login-form-container'>
+        <div className='login-header'>Login</div>
+        
+        <label for='email'><b>Email</b></label>
+        <input aria-label='email' type='text' placeholder='Enter Email' name='email' required/>
+
+        <label for='password'><b>Password</b></label>
+        <input aria-label='password' type='text' placeholder='Enter Password' name='password' required/>
+
+        <label>
+          <input aria-label='remember-me-checkbox' type='checkbox' checked='checked' name='remember' style={{marginRight: '0.5rem'}}/>
+          Remember me
+        </label>
+        <span className='login-forgot-password'>Forgot <a href="#">password?</a></span>
+        
+        <div className='login-buttons'>
+          <button type='submit-button' className='submit-button'>Login</button>
+          <button type='button' className='cancel-button'>Cancel</button> 
+        </div>
+        
+      </div>
+  )
+}
+
+export default Login
